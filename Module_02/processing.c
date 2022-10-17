6f2e40aca60e3a1b1bfb74fa4367db349eff4bd4
mdislammazharul <mazharul.py@gmail.com>
2022-10-17 08:37:21 +0200

    sum

---
+++ Module_02/processing.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter two Numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int sum;
+    sum = a + b;
+    printf("The Sum of the two numbers is %d", sum);
+    return 0;
+}
