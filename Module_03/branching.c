779c9616af097f1d94e8c82fa2f225a519818bd2
mdislammazharul <mazharul.py@gmail.com>
2022-10-18 22:06:42 +0200

    branching

---
+++ Module_03/branching.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("Please enter two numbers: ");
+    scanf("%d %d", &x,&y);
+
+    if(x>y)
+    {
+        printf("X is large.");
+    }
+    else
+    {
+        if(x<y)
+        {
+            printf("Y is large.");
+        }
+        else
+        {
+            printf("They are equal.");
+        }
+    }
+
+    return 0;
+}
